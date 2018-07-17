70743611e6e3dc90bdd1d1d1ca3371100531c020
Demo User <demouser@MacBook-Pro.local>
2018-07-17 16:34:25 -0500

    alarm clock lab

--- javaScript/labs/Alarm_Clock/script.js
+++ javaScript/labs/Alarm_Clock/script.js
@@ -13,3 +13,41 @@
 // limitations under the License.
 
 console.log("script is running...");
+
+function My_Alarm(time){
+  return "Hey, Sage, wake up! It's " + time;
+}
+
+function Mom_Alarm(time){
+  return "Hey, Mom, wake up! It's " + time;
+}
+
+function Family_Alarm(name, time){
+  return "Hey, " + name + ", wake up! It's " + time;
+}
+
+function Important_Alarm(message){
+  return message.toUpperCase();
+}
+
+function Snooze_Alarm(time){
+  var newTime = time + 1;
+  return "The alarm for " + time + " has been changed to " + newTime;
+}
+
+function Rep_Alarm(num){
+  for(i=num; i>0; i--){
+    console.log("Wake up!");
+  }
+}
+
+console.log(My_Alarm(10));
+console.log(Mom_Alarm(10));
+console.log(Family_Alarm("Dad", 10));
+console.log(Important_Alarm("wake up, wake up, wake UP!!"));
+console.log(Snooze_Alarm(10));
+Rep_Alarm(10);
+
+console.log("script has stopped.")
+
+// console.log('Hey ${time}');
