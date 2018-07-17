667cefed070eb3fea7f182dc3ee46f6693143d24
Demo User <demouser@MacBook-Pro.local>
2018-07-17 16:07:11 -0500

    file for testing js

---
+++ tester-js/javascript.js
@@ -0,0 +1,17 @@
+console.log('Hello World!');
+
+const name = 'Charlie';
+console.log('Hello '+ name);
+
+let age = 17;
+console.log('you are ' + age + ' years old');
+
+// while(age < 20){
+//   if(age < 20){
+//     console.log('yeah');
+//     age = age + 1;
+//   }
+//   else{
+//     console.log('na');
+//   }
+// }
