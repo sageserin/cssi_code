c06eea6fb0beb1a5dc5cb968e1b64b1be0c4d8ea
Demo User <demouser@MacBook-Pro.local>
2018-07-18 16:56:11 -0500

    annoying dog lab thank god its over

--- javaScript/labs/Extreme_Puppy_Generator/script.js
+++ javaScript/labs/Extreme_Puppy_Generator/script.js
@@ -13,3 +13,91 @@
 // limitations under the License.
 
 console.log(dataObject);
+
+let urlButton = document.getElementById('url0');
+let clearButton = document.getElementById('clear');
+let randButton = document.getElementById('rand');
+var gif0url = dataObject.data['0'].images.original.url;
+
+let nums = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+urlButton.addEventListener('click', e => {
+  // console.log(gif0url);
+  // var img0 = document.createElement("IMG");
+  // img0.src = gif0url;
+  // document.body.appendChild(img0);
+  nums.forEach((val, index) => {
+    var gifurl = dataObject.data[val].images.original.url;
+    var giftitle = dataObject.data[val].title;
+    console.log(gifurl);
+    var img = document.createElement("IMG");
+    var h = document.createElement('h1');
+    var t = document.createTextNode(giftitle);
+    h.appendChild(t);
+    img.src = gifurl;
+    img.height = '200';
+    img.width = '200';
+    img.classList.add('gif');
+    h.classList.add('title');
+    document.body.append(h);
+    document.body.append(img);
+  });
+});
+
+// var giferinos = [document.body.getElementsByClassName('gif')];
+var bad = document.querySelectorAll('.gif');
+
+clearButton.addEventListener('click', e => {
+  remove();
+});
+
+// randButton.addEventListener('click', e => {
+//   remove();
+  // var num = getRandom(0, 9);
+  // var gifurl = dataObject.data[num].images.original.url;
+  // var giftitle = dataObject.data[num].title;
+  // var img = document.createElement('img');
+  // var h = document.createElement('h1');
+  // var t = document.createTextNode(giftitle);
+  // h.appendChild(t);
+  // img.src = gifurl;
+  // img.height = '200';
+  // img.weight = '200';
+  // img.classList.add('gif');
+  // h.classList.add('title');
+  // document.body.append(h);
+  // document.body.append(img);
+// });
+
+function getRandom(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function mouseGif(e){
+  remove();
+  var num = getRandom(0, 9);
+  var gifurl = dataObject.data[num].images.original.url;
+  var giftitle = dataObject.data[num].title;
+  var img = document.createElement('img');
+  var h = document.createElement('h1');
+  var t = document.createTextNode(giftitle);
+  h.appendChild(t);
+  img.src = gifurl;
+  img.height = '200';
+  img.weight = '200';
+  img.classList.add('gif');
+  h.classList.add('title');
+  document.body.append(h);
+  document.body.append(img);
+}
+
+function remove() {
+  badG = document.querySelectorAll('.gif');
+  badH = document.querySelectorAll('.title');
+  badG.forEach((one, index) => {
+    one.remove();
+  });
+  badH.forEach((one, index) => {
+    one.remove();
+  });
+}
