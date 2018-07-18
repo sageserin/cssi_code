6d257a995031cc604d78875651a0914ca3ffe69f
Demo User <demouser@MacBook-Pro.local>
2018-07-18 11:31:32 -0500

    frogger lab

--- javaScript/labs/Frog_Hopper_DOMination/script.js
+++ javaScript/labs/Frog_Hopper_DOMination/script.js
@@ -12,14 +12,93 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-let currentlily = 1;
+var currentlily = 1;
 
-let frogger = /*use a querySelector to grab your frog from your HTML*/;
+let frogger = document.getElementById('frog'); /*use a querySelector to grab your frog from your HTML*/;
+let lilypad1 = document.getElementById('lilypad1');
+let lilypad2 = document.getElementById('lilypad2');
+let lilypad3 = document.getElementById('lilypad3');
+let lilypad4 = document.getElementById('lilypad4');
+let lilypad5 = document.getElementById('lilypad5');
 
-frogger.addEventListener(/* Insert type of event are we listening for */, function(){
+frogger.addEventListener('click', function(){
 // Insert what should happen when you click on the frog!
+  var lilynum = 'lilypad'+currentlily;
+  let lilypad = document.getElementById(lilynum);
+  console.log(lilypad);
 
+  console.log('hop');
 
+  (lilypad).classList.remove('active');
+  currentlily += 1;
+  lilynum = 'lilypad'+currentlily;
+  lilypad = document.getElementById(lilynum);
+  console.log(lilypad);
 
-
+  frog.style.left = lilypad.style.left;
+  frog.style.top = lilypad.style.top;
+  lilypad.classList.add('active');
 });
+
+frogger.addEventListener('mouseenter', function() {
+  frog.style.height = '80px';
+  frog.style.width = '80px';
+})
+
+frogger.addEventListener('mouseout', function() {
+  frog.style.height = '70px';
+  frog.style.width = '70px';
+})
+
+document.body.onkeyup = function(e){
+  if (e.which === 32){
+    frog.style.left = '17%';
+    frog.style.top = '50%';
+    lilypad1.classList.add('active');
+    lilypad2.classList.remove('active');
+    lilypad3.classList.remove('active');
+    lilypad4.classList.remove('active');
+    lilypad5.classList.remove('active');
+  }
+}
+
+// if (currentlily = 1) {
+//   frog.style.left = "33.5%";
+//   frog.style.top = "24%";
+//   lily1.classList.remove('active');
+//   lily2.classList.add('active');
+//   currentlily += 1;
+// }
+//
+// if (currentlily = 2) {
+//   frog.style.left = "50%";
+//   frog.style.top = "50%";
+//   lily2.classList.remove('active');
+//   lily3.classList.add('active');
+//   currentlily += 1;
+// }
+//
+// if (currentlily = 3) {
+//   frog.style.left = "68$";
+//   frog.style.top = "75%";
+//   lily3.classList.remove('active');
+//   lily4.classList.add('active');
+//   currentlily += 1;
+// }
+//
+// if (currentlily = 4) {
+//   frog.style.left = "83%";
+//   frog.style.top = "50%";
+//   lily4.classList.remove('active');
+//   lily5.classList.add('active');
+//   currentlily += 1;
+// }
+
+
+
+// for(i=2; i<6; i++){
+//   frog.style.left = 'lily'+'i'.style.left;
+//   frog.style.right = lily+'i'.style.right;
+//   lily+('i'-1).classList.remove('active');
+//   lily+'i'.classList.add('active');
+// }
