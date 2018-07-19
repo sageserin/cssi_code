bf4a3d5498ec3cd7d4f8eced97beae59397b1a14
Demo User <demouser@MacBook-Pro.local>
2018-07-19 10:31:38 -0500

    fun w anime.js

---
+++ animetest.js
@@ -0,0 +1,8 @@
+var cssProperties = anime({
+  targets: '#cssProperties .here',
+  opacity: .5,
+  left: '240px',
+  backgroundColor: '#7897c9',
+  borderRadius: ['0em', '2em'],
+  easing: 'easeInOutQuad'
+});
