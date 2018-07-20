56b7ae0e7482a6e35e58e5b7b5ac5e5f055b153b
Demo User <demouser@MacBook-Pro.local>
2018-07-20 12:30:17 -0500

    movie reviewer lab

--- python/labs/movie-review-database-update/starter-code/movie_reviewer.py
+++ python/labs/movie-review-database-update/starter-code/movie_reviewer.py
@@ -17,11 +17,12 @@
 inside_movie = {
     "title": "Inside Out",
     "id": "tt2096673",
-    "year_released": 2012,
+    "year_released": 2012, #2015
     "rating": "PG",
-    "score": 7.5,
-    "out_of": 10,
-    "reviews": 463787
+    "score": 7.5, #8.2
+    "out_of": 10, #remove
+    "reviews": 463787 #489496
+    #add genres: animation, adventure, comedy
 }
 
 # Do not edit the code above!
@@ -29,3 +30,16 @@ inside_movie = {
 # Write your code below to update the information in accordance with its
 # IMDB page: http://www.imdb.com/title/tt2096673/
 
+# for x, y in my_dict.items():
+#     print('Key: {k}, Value: {v}'.format(k=x, v=y))
+
+inside_movie['year_released'] = 2015
+inside_movie['score'] = 8.2
+del inside_movie['out_of']
+inside_movie['reviews'] = 489496
+inside_movie['genres'] = 'animation, adventure, comedy'
+
+for x, y in inside_movie.items():
+    print('Key: {k}, Value: {v}'.format(k=x, v=y))
+
+print(inside_movie)
