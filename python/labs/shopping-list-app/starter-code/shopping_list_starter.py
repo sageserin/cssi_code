96b3db1a153e71b0974dfe65b966daa4696fcef5
Demo User <demouser@MacBook-Pro.local>
2018-07-20 12:30:44 -0500

    shopping list lab

--- python/labs/shopping-list-app/starter-code/shopping_list_starter.py
+++ python/labs/shopping-list-app/starter-code/shopping_list_starter.py
@@ -30,56 +30,58 @@ while choice.lower() != "e":
     print("d. Show all items on the list")
     print("e. exit")
 
-    choice = input("Enter your choice [a|b|c|d|e]: ")
+    choice = input("Enter your choice [a|b|c|d|e]: ").lower()
 
     # Your code below! Handle the cases when the user chooses a, b, c, d, or e
 
     if choice == 'a':
         add = input('What do you want to add?: ')
-        if not (choice in shopping_list):
-            # add = add.split().strip()
-            shopping_list.append(add)
-            print('Added! Here is your updated shopping list: ')
-            print(shopping_list)
-        else:
+        if add in shopping_list:
             print('Sorry, that item is already on your list!')
+        else:
+            add = add.split(',')
+            for item in add:
+                item.strip()
+                shopping_list.append(item)
+            print('Added!')
+
     elif choice == 'b':
         remove = input('What do you want to remove?: ')
         if remove in shopping_list:
             valid = input('Are you sure you want to remove this item? [yes|no]: ')
             if valid == 'yes':
                 shopping_list.remove(remove)
-                print('Removed! Here is your updated shopping list: ')
-                print(shopping_list)
+                print('Removed!')
             if valid == 'no':
                 print('Alright, disaster averted.')
         else:
             print('Sorry, you\'re trying to remove something that isn\'t on your list!')
+
     elif choice == 'c':
         check = input('What do you want to look for?: ')
         if check in shopping_list:
             print('{checked} is already on your shopping list!'.format(checked = check))
             destroy = input('Would you like to remove {checked} from your list? [yes/no]: '.format(checked = check))
             if destroy == 'yes':
-                shopping_list.remove(destroy)
-                print('Removed! Here is your updated shopping list:')
-                print(shopping_list)
+                shopping_list.remove(check)
+                print('Removed!')
             else:
                 print('Alright, disaster averted.')
         else:
             print('{checked} isn\'t on your shopping list yet!'.format(checked = check))
             create = input('Would you like to add {checked} to your list? [yes/no]: '.format(checked = check))
             if create == 'yes':
-                shopping_list.append(create)
-                print('Added! Here is your updated shopping list:')
-                print(shopping_list)
+                shopping_list.append(check)
+                print('Added!')
             else:
                 print('Alright, maybe next time.')
+
     elif choice == 'd':
-        print('Here is your shopping list: ')
-        print(shopping_list)
+        print('Generating...')
+
     elif choice == 'e':
         print('Okay! See ya later.')
         break
+
     else:
         choice = input('That is not an option! Please choose from [a|b|c|d|e]: ')
