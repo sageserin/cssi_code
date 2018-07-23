1b63e1a3948118afbb582dd0daeb52bde25e0a98
Demo User <demouser@MacBook-Pro.local>
2018-07-23 08:55:33 -0500

    banking lab

--- python/labs/quick-banking-app/starter-code/banking.py
+++ python/labs/quick-banking-app/starter-code/banking.py
@@ -17,4 +17,46 @@
 # Replace "pass" with your code
 
 class BankAccount(object):
-    pass
+    def __init__(self, label, balance):
+        self.label = label
+        self.balance = balance
+
+    def __str__(self):
+        info = 'label: {l}, balance: {b}'.format(l=self.label, b=self.balance)
+        return info
+
+    def withdraw(self, val):
+        bal = self.balance
+        if val <= bal and val > 0:
+            self.balance -= val
+            statement = 'your new balance is {b}'.format(b=self.balance)
+        elif val < 0:
+            statement = 'you can\'t withdraw a negative amount'
+        else:
+            statement = 'not enough money to withdraw that amount'
+        return statement
+
+    def deposit(self, val):
+        if val > 0:
+            self.balance += val
+            statement = 'your new balance is {b}'.format(b=self.balance)
+        else:
+            statement = 'you can\'t deposit a negative amount'
+        return statement
+
+    def rename(self, name):
+        if name == "":
+            statement = 'the name of your account cannot be blank'
+        else:
+            self.label = name
+            statement = 'the new name of your account is {n}'.format(n=name)
+        return statement
+
+    def transfer(self, dest_acc, amount):
+        if amount < self.balance and amount > 0:
+            dest_acc.balance += amount
+            self.balance -= amount
+            statement = 'successful'
+        else:
+            statement = 'invalid'
+        return statement
