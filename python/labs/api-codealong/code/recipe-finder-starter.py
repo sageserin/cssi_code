de49304281a13ff18b07f09cc7b6ea32c32e39c3
Demo User <demouser@MacBook-Pro.local>
2018-07-23 08:54:48 -0500

    recipe finder lab

--- python/labs/api-codealong/code/recipe-finder-starter.py
+++ python/labs/api-codealong/code/recipe-finder-starter.py
@@ -13,16 +13,28 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import requests #pip install [library name]
 
 ingredients = []
 
-enter_more = input("Do you have any specific ingredients to enter? [y|n]:").lower()
+enter_more = input("Do you have any specific ingredients to enter? [y|n]: ").lower()
 
 while enter_more == "y":
-    ingredients.append(input("What is the ingredient? One word only please:").lower())
-    enter_more = input("Do you have any more ingredients to enter? [y|n]:").lower()
+    ingredients.append(input("What is the ingredient? One word only please: ").lower())
+    enter_more = input("Do you have any more ingredients to enter? [y|n]: ").lower()
 
-recipe = input("What kind of recipe do you want to find?")
+recipe = input("What kind of recipe do you want to find? ")
 
 # Write your code below!
 
+print(ingredients)
+print(recipe)
+
+ing = ','.join(ingredients)
+api_string = 'http://www.recipepuppy.com/api/?i={ingredients}&q={recipe_type}'
+new_string = api_string.format(ingredients=ing, recipe_type=recipe)
+
+print(new_string)
+
+r = requests.get(new_string)
+print(r.json())
